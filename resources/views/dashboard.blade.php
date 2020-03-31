3a5a93c56285737e803aa6b013ab18161fe16334
qkoh st <kukohsantoso013@gmail.com>
2020-03-31 09:50:53 +0700

    Layout Dashboard

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,249 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>PPDB Online SMP DATA</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Scaffold - v2.0.0
+  * Template URL: https://bootstrapmade.com/scaffold-bootstrap-metro-style-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex">
+
+      <div class="logo mr-auto">
+        <h1 class="text-light"><a href="index.html"><span>PPDB Online</span></a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li class="active"><a href="/">Home</a></li>
+          <li><a href="https://forms.gle/TcofPQWxwZYyEJXF8">Daftar</a></li>
+        </ul>
+      </nav><!-- .nav-menu -->
+
+      <div class="header-social-links">
+        <a href="#" class="twitter"><i class="icofont-twitter"></i></a>
+        <a href="#" class="facebook"><i class="icofont-facebook"></i></a>
+        <a href="#" class="instagram"><i class="icofont-instagram"></i></a>
+        <a href="#" class="linkedin"><i class="icofont-linkedin"></i></i></a>
+      </div>
+
+    </div>
+  </header><!-- End Header -->
+
+    <!-- ======= Hero Section ======= -->
+    <section id="hero">
+
+    <div class="container">
+    <div class="row">
+        <div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center" data-aos="fade-up">
+        <div>
+            <h1>Selamat Datang Di Website PPDB Online</h1>
+            <h2>SMP Darut Tauhid Tambakboyo Tahun Pelajaran 2020/2021</h2>
+            <a href="https://forms.gle/TcofPQWxwZYyEJXF8" class="btn-get-started scrollto">Daftar Sekarang</a>
+        </div>
+        </div>
+        <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="fade-left">
+        <img src="assets/img/daftar.png" class="img-fluid" alt="">
+        </div>
+    </div>
+    </div>
+
+    </section><!-- End Hero -->
+
+    <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+    <div class="container">
+
+        <div class="row">
+        <div class="col-lg-6" data-aos="zoom-in">
+            <img src="assets/img/brosur_ppdb.jfif" class="img-fluid" alt="">
+        </div>
+        <div class="col-lg-6 d-flex flex-column justify-contents-center" data-aos="fade-left">
+            <div class="content pt-4 pt-lg-0">
+            <h3>Pendaftaran Online</h3>
+            <p class="font-italic">
+                Ditengah merebaknya pandemi virus Covid-19 di Indonesia akhir-akhir ini. Untuk memudahkan calon peserta didik baru dalam melakukan pendaftaran, maka kami meyediakan pelayanan pendaftaran peserta didik baru mode online atau daring dengan alur pendaftaran sebagai berikut :  
+            </p>
+            <ul>
+                <li><i class="icofont-check-circled"></i> Mengisi formulir pendaftaran online dengan melampirkan foto/scan berkas persyaratan</li>
+                <li><i class="icofont-check-circled"></i> Mengikuti test observasi/masuk online sesuai dengan jadwal yang telah ditentukan</li>
+                <li><i class="icofont-check-circled"></i> Menerima pengumuman</li>
+                <li><i class="icofont-check-circled"></i> Melakukan daftar ulang</li>
+                <li><i class="icofont-check-circled"></i> Masuk kegiatan belajar mengajar</li>
+            </ul>
+            <p>
+                Selain pendaftaran mode online atau daring, kami juga masih melayani pendaftaran offline/luring dengan datag langsung ke sekretariat pendaftaran (Kantor SMP Darut Tauhid Tambakboyo).
+            </p>
+            </div>
+        </div>
+        </div>
+
+    </div>
+    </section><!-- End About Section -->
+
+    <!-- ======= Team Section ======= -->
+    <!-- <section id="team" class="team">
+    <div class="container">
+
+        <div class="section-title" data-aos="fade-up">
+        <h2>Dewan Pengajar</h2>
+        <p>Tenaga pengajar yang berkompeten dalam bidangnya</p>
+        </div>
+
+        <div class="row">
+
+        <div class="col-lg-4 col-md-6">
+            <div class="member" data-aos="zoom-in">
+            <div class="pic"><img src="assets/img/team/team-1.jpg" class="img-fluid" alt=""></div>
+            <div class="member-info">
+                <h4>Moch. Munir, S.E.</h4>
+                <span>Kepala Sekolah</span>
+                <div class="social">
+                <a href=""><i class="icofont-twitter"></i></a>
+                <a href=""><i class="icofont-facebook"></i></a>
+                <a href=""><i class="icofont-instagram"></i></a>
+                <a href=""><i class="icofont-linkedin"></i></a>
+                </div>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6">
+            <div class="member" data-aos="zoom-in" data-aos-delay="100">
+            <div class="pic"><img src="assets/img/team/team-2.jpg" class="img-fluid" alt=""></div>
+            <div class="member-info">
+                <h4>Habibul Lubab, S.Pd.</h4>
+                <span>Waka Kurikulum & Guru Bahasa Inggris</span>
+                <div class="social">
+                <a href=""><i class="icofont-twitter"></i></a>
+                <a href=""><i class="icofont-facebook"></i></a>
+                <a href=""><i class="icofont-instagram"></i></a>
+                <a href=""><i class="icofont-linkedin"></i></a>
+                </div>
+            </div>
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6">
+            <div class="member" data-aos="zoom-in" data-aos-delay="200">
+            <div class="pic"><img src="assets/img/team/team-3.jpg" class="img-fluid" alt=""></div>
+            <div class="member-info">
+                <h4>Ali Ghufron, S.Pd.</h4>
+                <span>Waka Kesiswaan & Guru IPS</span>
+                <div class="social">
+                <a href=""><i class="icofont-twitter"></i></a>
+                <a href=""><i class="icofont-facebook"></i></a>
+                <a href=""><i class="icofont-instagram"></i></a>
+                <a href=""><i class="icofont-linkedin"></i></a>
+                </div>
+            </div>
+            </div>
+        </div>
+
+        </div>
+
+    </div>
+    </section> -->
+
+    <!-- End Team Section -->
+    </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-6">
+            <div class="footer-info">
+              <h3>SMP Darut Tauhid Tambakboyo</h3>
+              <p>
+               Alamat : Jl. KH. Ashary Pabeyan Tambakboyo Tuban <br>
+                <strong>HP :</strong> 0852-0456-3827<br>
+                <strong>Email :</strong> s.daruttauhid@yahoo.com<br>
+              </p>
+              <div class="social-links mt-3">
+                <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+                <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+                <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 footer-newsletter">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6662.81052201679!2d111.83356195314701!3d-6.8025630204564544!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x209ccb1c8695a44e!2sSMP%20Darut%20Tauhid%20Tambakboyo!5e0!3m2!1sid!2sid!4v1585570723554!5m2!1sid!2sid" width="370" height="250" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>            <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" style="border:0; width: 100%; height: 250px;" allowfullscreen></iframe> -->
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>2020</span></strong>. SMP Darut Tauhid Tambakboyo
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/scaffold-bootstrap-metro-style-template/ -->
+        Developed by <a href="https://bootstrapmade.com/">Qkoh St.</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top"><i class="bx bxs-up-arrow-alt"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
